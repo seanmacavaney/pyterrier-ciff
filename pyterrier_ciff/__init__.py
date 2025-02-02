8e6bcb1672658c380326aedd837deb126ddea3a3
Sean MacAvaney <sean.macavaney@gmail.com>
2025-02-02 16:53:13 +0000

    bump version

--- pyterrier_ciff/__init__.py
+++ pyterrier_ciff/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for PyTerrier CIFF."""
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 from pyterrier_ciff._ciff_pb2 import DocRecord, Header, Posting, PostingsList # noqa: I001
 from pyterrier_ciff._invert import invert
